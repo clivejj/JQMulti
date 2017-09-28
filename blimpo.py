b06048aef6a8fa65c65367c0edae54829d63901b
clivejj <cjohnston1@stuy.edu>
2017-09-28 19:30:07 -0400

    new organization structure

--- blimpo.py
+++ blimpo.py
@@ -1,37 +1,8 @@
 import random
 from flask import Flask, render_template
+from utils import csv
 
-
-def csvToDict(csvName):
-    #read csv file
-    reader = open(csvName, "r")
-    data = reader.read()
-    #print data
-    reader.close()
-
-    #split csv file by line
-    dataList = data.splitlines()
-    #print dataList
-    dataList = dataList[:-1]
-    #print dataList
-
-    dict = {}
-    for x in dataList[1:]:
-        #deal with case of commas within quotations
-        if x[0] == '"':
-            #split by quotations
-            temp = x.split('"')
-            #convert to float
-            dict[temp[1]] = [float(temp[2].split(",")[1]), temp[2].split(",")[2]]
-            #normal case
-        else:
-            temp = x.split(",")
-            #convert to float
-            dict[temp[0]] = [float(temp[1]), temp[2]]
-    #dict.pop("Total")
-    return dict
-
-dict = csvToDict("occupations.csv")
+dict = csv.csvToDict("data/occupations.csv")
 
 def randOccupation():
     num = random.uniform(0, 99.8)
---
+++ utils/csv.py
@@ -0,0 +1,28 @@
+def csvToDict(csvName):
+    #read csv file
+    reader = open(csvName, "r")
+    data = reader.read()
+    #print data
+    reader.close()
+
+    #split csv file by line
+    dataList = data.splitlines()
+    #print dataList
+    dataList = dataList[:-1]
+    #print dataList
+
+    dict = {}
+    for x in dataList[1:]:
+        #deal with case of commas within quotations
+        if x[0] == '"':
+            #split by quotations
+            temp = x.split('"')
+            #convert to float
+            dict[temp[1]] = [float(temp[2].split(",")[1]), temp[2].split(",")[2]]
+            #normal case
+        else:
+            temp = x.split(",")
+            #convert to float
+            dict[temp[0]] = [float(temp[1]), temp[2]]
+    #dict.pop("Total")
+    return dict
